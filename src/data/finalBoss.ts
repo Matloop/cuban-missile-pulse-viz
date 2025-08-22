48bed1060da25f7e3ae8be5069ebc5fa6f2f4c79
Matlooper <matheusdiasestacioo@gmail.com>
2025-08-22 14:10:34 -0300

    boss

--- src/data/finalBoss.ts
+++ src/data/finalBoss.ts
@@ -8,6 +8,6 @@ export const theFinalBoss: HistoricalFigure = {
   chance: 0, // Não é para ser encontrado em lootbox
   description: "O astuto e imprevisível Premier soviético. O confronto final pela paz mundial depende de você decifrar suas intenções e superá-lo.",
   image: "minatto.jpg", // Certifique-se de ter essa imagem!
-  stats: { hp: 350, attack: 10, speed: 70 } // Stats elevados para um chefão final
+  stats: { hp: 550, attack: 30, speed: 70 } // Stats elevados para um chefão final
 };
 
