267bfc65b50dc309b4bdc70aae163b041df82025
MATHEUS DIAS ESTACIO <matheus_estacio@estudante.sesisenai.org.br>
2025-08-25 09:29:16 -0300

    boss

--- src/data/finalBoss.ts
+++ src/data/finalBoss.ts
@@ -3,7 +3,7 @@ import { HistoricalFigure } from '../types/crisisDataTypes'; // Ajuste o caminho
 
 export const theFinalBoss: HistoricalFigure = {
   id: "nikita_khrushchev_final_boss",
-  name: "Nikita Khrushchev",
+  name: "Gustev Mineta",
   rarity: "Lendário", // Propriedade obrigatória para HistoricalFigure
   chance: 0, // Não é para ser encontrado em lootbox
   description: "O astuto e imprevisível Premier soviético. O confronto final pela paz mundial depende de você decifrar suas intenções e superá-lo.",
